7ffb5e60d4caeee2e87b546f69fa369bbe088e9d
Daniel Nephin <dnephin@gmail.com>
2013-10-05 02:42:31 -0600

    Vagrant and dev/bootstrap.

--- Vagrantfile
+++ Vagrantfile
@@ -14,6 +14,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   # Port forwarding for grunt livereload server
   config.vm.network "forwarded_port", guest: 35729, host: 8081
+  # TODO: not working
+  # Port forward for couchdb server
+  config.vm.network "forwarded_port", guest: 5984,  host: 8082
 
   config.vm.hostname = "quill-dev"
 
--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -5,9 +5,30 @@
 
 set -e
 
+# Install english language pack
+sudo locale-gen en_US.UTF-8
+
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update -qq -y
 sudo apt-get install -y nodejs
+
+# TODO: set this to run at startup
+# TODO: set bind_address to 0
+sudo apt-get install -y couchdb
+# TODO: store with views
+curl -X PUT http://localhost:5984/statement
+
+
+# TODO: replace with smaller binaries
+# Scala and Play
+sudo apt-get install unzip
+get http://downloads.typesafe.com/typesafe-activator/1.0.0/typesafe-activator-1.0.0.zip
+unzip typesafe-activator-1.0.0.zip
+cd activator-1.0.0
+
+curl http://www.scala-lang.org/files/archive/scala-2.10.2.tgz | tar -x && \
+    cd scala-2.10.2
+
 
 # Create a link from the users home directory to the shared folder
 ln -sfT /vagrant project-quill
