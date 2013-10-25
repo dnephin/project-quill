b4f5d34b31da5df2e36c617387a900159cff1b13
Daniel Nephin <dnephin@gmail.com>
2013-10-25 00:38:42 -0600

    Some minor WIP.

--- web_backend/app/dao/StatementData.scala
+++ web_backend/app/dao/StatementData.scala
@@ -12,10 +12,37 @@ object StatementData {
 
     // TODO: config
     val url = "http://localhost:5984/statement"
+
+    var currentPublishedView = s"$url/_design/app/_view/current_published"
+    
+    var currentView = s"$url/_design/app/_view/current"
     
     def add(stmt: StatementModel): Future[Response] = {
         // TODO: check version
         // TODO: check _id is not present
         WS.url(url).post(Json.toJson(stmt))
+    }
+
+    def getCurrentPublished(label: String) = {
+        WS.url(currentPublishedView)
+            .withQueryString("group" -> "true", "key" -> label)
+            .get()
+            .map {
+                response =>
+                    // TODO: extract response handling
+                    // TODO: raise 404 on missing
+                    val id = (response.json \ "rows")(1)(__ \ "value").as[String]
+                    getById(id)
+            }
+    }
+
+    /**
+      * Retrieve a Statement by id.
+      */
+    def getById(id: String) = {
+        WS.url(s"$url/$id").get().map {
+            response =>
+                response.json.validate[StatementModel]
+        }
     }
 }
