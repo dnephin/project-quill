8776dc6d8ce2efba0d209e1e966f174f1037fd89
Daniel Nephin <dnephin@gmail.com>
2013-11-30 13:49:50 -0700

    Adding a first draft of a possible ApiBodyParser.

--- web_backend/app/components/ApiAction.scala
+++ web_backend/app/components/ApiAction.scala
@@ -1,14 +1,28 @@
 package quill.components
 
+
+import play.api.mvc.{ BodyParsers, BodyParser }
+
+
 /**
   * Uses:
   * * BodyParser - [[play.api.mvc.BodyParsers.parser.json]]
   *
   */
-object ApiAction {
+class ApiBodyParser[T] with BodyParsers {
 
-    def apply() = {
-
+    def apply: BodyParser[T] = json.flatMap { jsValue =>
+        jsValue.validate[T] match {
+            case JsSuccess(item) => {
+                // TODO: is there an easier helper?
+                tolerantBodyParser[T](T.toString(), DEFAULT_MAX_TEXT_LENGTH, "Bad Request") {
+                    (request, bytes) => item
+                }
+            }
+            // TODO: json error message with validation error
+            case e: JsError => error(BadRequest(e.toString))
+        }
     }
 
 }
+
